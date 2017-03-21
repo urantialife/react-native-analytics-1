14ca03f5c0a9bf57c788a78039c5013d5fade724
Jessica Lo <jessicalo92@gmail.com>
2017-03-21 14:04:17 -0700

    add alias support

--- ios/RNAnalytics/RNSegmentIOAnalytics.m
+++ ios/RNAnalytics/RNSegmentIOAnalytics.m
@@ -30,6 +30,13 @@ RCT_EXPORT_METHOD(identify:(NSString*)userId traits:(NSDictionary *)traits) {
 }
 
 /*
+ https://segment.com/docs/libraries/ios/#alias
+ */
+RCT_EXPORT_METHOD(alias:(NSString*)userId) {
+    [[SEGAnalytics sharedAnalytics] alias:userId];
+}
+
+/*
  https://segment.com/docs/libraries/ios/#track
  */
 RCT_EXPORT_METHOD(track:(NSString*)event properties:(NSDictionary *)properties) {
